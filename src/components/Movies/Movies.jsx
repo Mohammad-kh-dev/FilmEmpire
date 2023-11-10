da9ca749a26554d362cf8e24ef39c56753dbb2a3
Mohammad Alkhattab <mohammad.alkhattab77@gmail.com>
2023-11-10 20:19:10 +0200

    Uoload

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,46 +1,81 @@
-import React, { useState } from 'react'
-import { useGetMoviesQuery } from '../../services/TMDB';
-import { useMediaQuery } from '@mui/material';
-import MovieList from '../MovieList/MovieList';
-import {Box,Typography,CircularProgress,Pagination} from '@mui/material';
-import Stack from '@mui/material/Stack';
- const Movies = () => {
-  
-  const { data, error, isFetching } = useGetMoviesQuery();
-  const lg = useMediaQuery((theme) => theme.breakpoints.only('lg'));
-  const numberOfMovies = lg ? 17 : 19;
+/* eslint-disable no-undef */
+import React, { useEffect } from "react";
+import { useGetMoviesQuery } from "../../services/TMDB";
+
+import MovieList from "../MovieList/MovieList";
+import { Box, Typography, CircularProgress, Pagination } from "@mui/material";
+import { useSelector } from "react-redux";
+import Stack from "@mui/material/Stack";
+import FillterMovie from "./FillterMovie";
+import { useDispatch } from "react-redux";
+import { fetchMoviesSuccess } from "../../features/MovieSlice";
+import FeaturedMovie from "./FeaturedMovie";
+const Movies = () => {
+  const { genreIDOrCategoryName,searchQuery,years } = useSelector(
+    (state) => state.currentGenerOrCategory
+  );
+ 
   const [page, setPage] = React.useState(1);
+    
+  const { data, error, isFetching } = useGetMoviesQuery({
+    genreIDOrCategoryName,
+    page,
+    searchQuery,
+    years
+  });
+  console.log(data);
+  const dispatch = useDispatch();
   const handleChange = (event, value) => {
     setPage(value);
   };
+  useEffect(() => {
+    dispatch(fetchMoviesSuccess(data));
+  }, [data, dispatch]);
 
-  if(isFetching){
-    return(
-      <Box display={'flex'} justifyContent={'center'}>
-        <CircularProgress/>
+  if (isFetching) {
+    return (
+      <Box display={"flex"} justifyContent={"center"}>
+        <CircularProgress />
       </Box>
-    )
+    );
   }
-    if(error){
-      return(
-      <Box display={'flex'} justifyContent={'center'}>
-        <Typography variant='h2'>
-          There Was an Error !
-        </Typography>
+  if (error) {
+    return (
+      <Box display={"flex"} justifyContent={"center"}>
+        <Typography variant="h2">There Was an Error !</Typography>
       </Box>
-      )
-    }
+    );
+  }
   return (
-    <div>
-    
-    <MovieList movies={data} numberOfMovies={numberOfMovies}  />
-    <Stack spacing={2}>
-      <Pagination count={data.results.length} shape="rounded" page={page} onChange={handleChange}  />
-      
-    </Stack>
-  </div>
-  )
+    <Box display={"flex"} flexDirection={"column"}>
+      <FeaturedMovie movie={data?.results[0]}/>
+      <Box
+        display={"flex"}
+        width={"100%"}
+        justifyContent={"space-around"}
+        marginTop={"1rem"}
+      >
+        <FillterMovie />
+      </Box>
+      <Box display={"flex"} flexDirection={"column"}>
+        <MovieList movies={data} />
+        <Stack
+          spacing={2}
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          paddingBottom={"1rem"}
+        >
+          <Pagination
+            count={data.results.length}
+            shape="rounded"
+            page={page}
+            onChange={handleChange}
+          />
+        </Stack>
+      </Box>
+    </Box>
+  );
 };
-
 
 export default Movies;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,125 +1,183 @@
 /* eslint-disable jsx-a11y/alt-text */
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
 
+
+import Box from "@mui/material/Box";
+import genresIcons from "../assets/genres";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+
+import { Link } from "react-router-dom";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+
+import ListItemText from "@mui/material/ListItemText";
+import { ListItemIcon } from "@mui/material";
+
+import Toolbar from "@mui/material/Toolbar";
+
+import { useGetGenresQuery } from "../services/TMDB";
+import {  ListSubheader } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { selectGenreOrCategory } from "../features/currentGenerOrCategory";
+import SearchAppBar from "./search/Search";
 const drawerWidth = 240;
-const blueLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
+const blueLogo =
+  "https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png";
 const NavBar = () => {
   const [mobileOpen, setMobileOpen] = React.useState(false);
-
+  
+  const [selectedGenre, setSelectedGenre] = React.useState(null);
+  const categories = [
+    { label: "Popular", value: "popular" },
+    { label: "Top Rated", value: "top_rated" },
+    { label: "Upcoming", value: "upcoming" },
+  ];
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
+  const { data } = useGetGenresQuery();
+  const dispatche = useDispatch();
+  const handleGenresQuery = (id) => {
+    
+    dispatche(selectGenreOrCategory(id))
+    setSelectedGenre(id);
 
+  };
+  const GenresList = (
+    <List>
+      <ListSubheader>{"genres".toUpperCase()}</ListSubheader>
+      {data?.genres.map(({ id, name }) => (
+        
+        <Link
+          key={name}
+          to={"/"}
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <ListItem key={id} disablePadding>
+          <ListItemButton
+        onClick={() => handleGenresQuery(id)}
+        sx={{
+          backgroundColor: selectedGenre === id ? "lightblue" : "transparent",
+          transition: "background-color 0.3s ease",
+        }}
+        className={(selectedGenre === id)? "active" : "disabled"}
+      >
+            
+              
+              <ListItemIcon>
+                <img
+                  src={genresIcons[name.toLowerCase()]}
+                  height={30}
+                  width={30}
+                />
+              </ListItemIcon>
+              <ListItemText primary={name} />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+    
+   
+      ))}
+    </List>
+  );
+  const CategaryList = (
+    <List>
+      {categories.map(({ label, value }) => (
+        <Link
+          key={label}
+          to={"/"}
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <ListItem disablePadding>
+            <ListItemButton
+              onClick={() => dispatche(selectGenreOrCategory(value))}
+            >
+              <ListItemIcon>
+                <img
+                  src={genresIcons[label.toLowerCase()]}
+                  height={30}
+                  width={20}
+                />
+              </ListItemIcon>
+              <ListItemText primary={label} />
+            </ListItemButton>
+          </ListItem>
+        </Link>
+      ))}
+    </List>
+  );
   const drawer = (
     <div>
-      <Toolbar>
-        <Box justifyContent={'center'} alignItems={'center'} >
-        <img  src={blueLogo} width={200} style={{margin:0,padding:0}}/>
+      <Toolbar >
+        <Box justifyContent={"center"} alignItems={"center"} sx={{overflow:'hidden'}}  >
+          <Link to={"/"} >
+            <img
+              src={blueLogo}
+              width={200}
+              height={100}
+              style={{ margin: 0, padding: 0 }}
+            />
+          </Link>
         </Box>
       </Toolbar>
       <Divider />
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      <ListSubheader>{"categories".toUpperCase()}</ListSubheader>
+      {CategaryList}
       <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      {GenresList}
     </div>
   );
-  return <Box>
-  
-  <AppBar
-    position="fixed"
-    sx={{
-      width: { sm: `calc(100% - ${drawerWidth}px)` },
-      ml: { sm: `${drawerWidth}px` },
-    }}
-  >
-    <Toolbar>
-      <IconButton
-        color="inherit"
-        aria-label="open drawer"
-        edge="start"
-        onClick={handleDrawerToggle}
-        sx={{ mr: 2, display: { sm: 'none' } }}
-      >
-        <MenuIcon />
-      </IconButton>
-      <Typography variant="h6" noWrap component="div">
-        Responsive drawer
-      </Typography>
-    </Toolbar>
-  </AppBar>
-  <Box
+
+
+  return (
+    <Box sx={{width:{ sm:drawerWidth}}}>
+      <SearchAppBar handleDrawerToggle={handleDrawerToggle} />
+      <Box
     component="nav"
     sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
     aria-label="mailbox folders"
   >
-    {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-    <Drawer
-      container={undefined}
-      variant="temporary"
-      open={mobileOpen}
-      onClose={handleDrawerToggle}
-      ModalProps={{
-        keepMounted: true, // Better open performance on mobile.
-      }}
-      sx={{
-        display: { xs: 'block', sm: 'none' },
-        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-      }}
-    >
-      {drawer}
-    </Drawer>
-    <Drawer
-      variant="permanent"
-      sx={{
-        display: { xs: 'none', sm: 'block' },
-        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-      }}
-      open
-    >
-      {drawer}
-    </Drawer>
-  </Box>
- 
-    <Toolbar /></Box>;
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={undefined}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+            
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+
+      <Toolbar />
+    </Box>
+  );
 };
+
 
 export default NavBar;
